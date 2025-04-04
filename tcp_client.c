6836126e825a7640221ee8d4b55b2e1acd0e012a
dithier <carter.ithier@gmail.com>
2025-04-04 11:26:05 -0400

    add comments to server/client code

--- tcp_client.c
+++ tcp_client.c
@@ -9,33 +9,45 @@
 
 #define MAX_LINE 256
 
+// main takes 3 arguments: the executable name, the server host (which will
+// likely be 127.0.0.1 -- localhost) and the port of the server you are
+// connecting to
 int main(int argc, char* argv[]) {
+    // a struct pointer that describes the internet host referenced by name. In
+    // general has info about host
     struct hostent* hp;
+    // host is a string representing the host ip address given by command line
+    // arg
+    // 2
     char* host;
-    // port that we will use
+    // port that for the server we will use which is given by command line arg 3
     int server_port;
 
-    // define other variables used for later
+    // a buffer or sending and recieving messages
     char buf[MAX_LINE];
+    // the length of the message/buffer used
     int len;
 
     if (argc != 3) {
         printf("Usage: ./tcp_client <server_host>  <server_port>\n");
         return -1;
     } else {
+        // save our command line args to variables
         host = argv[1];
         server_port = atoi(argv[2]);
     }
 
+    // initialize our host info by looking up host ip address
     hp = gethostbyname(host);
     if (!hp) {
-        fprintf(stderr, "tcp_client: unknwon host %s\n", host);
+        fprintf(stderr, "tcp_client: unknown host %s\n", host);
         exit(1);
     }
 
     // our client will connect to a server with the following address and port
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
+    // AF_INET is IPv4
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(server_port);
     strcpy(hp->h_addr_list[0], (char*)&server_address.sin_addr);
@@ -44,11 +56,12 @@ int main(int argc, char* argv[]) {
     int client_socket;
 
     // Initalize socket and do error checking
+    // SOCK_STREAM is TCP
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\nSocket creation error\n");
     }
 
-    // attempt to connect
+    // attempt to connect to server
     int connection_status;
 
     if ((connection_status =
@@ -73,6 +86,8 @@ int main(int argc, char* argv[]) {
         fputs("Server says: ", stdout);
         fputs(buf, stdout);
     }
+
+    close(client_socket);
 
     return 0;
 }
--- tcp_server.c
+++ tcp_server.c
@@ -12,13 +12,24 @@
 
 // max number of client connections that can be queued to join
 #define MAX_CLIENTS 5
+// max buffer size for message
 #define MAX_LINE 256
 
+// main takes 2 args: the executable name and the port number
+// for the server
 int main(int argc, char *argv[]) {
+    // port we are using for communication
     int port;
+    // the socket we will use to communicate with client
     int client_socket;
+    // the amount of space pointed to by the address in the
+    // accept call and on return will have th actual length (in bytes)
+    // of the address returned (see use in while loop below)
     int addr_len;
+    // the buffer we will be storing the client's request in
     char buf[MAX_LINE];
+    // the length of the buffer used that  we will be storing the client's
+    // request in
     int buf_len;
 
     if (argc != 2) {
@@ -29,18 +40,23 @@ int main(int argc, char *argv[]) {
     }
 
     // define the server address format
+    // server_address is struct to store server details
     struct sockaddr_in server_address;
+    // AF_INET specifies IPv4
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
+    // INADDR_ANY allows server to accept connections on any available network
+    // interface
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
     // create the server socket
     int server_socket;
+    // SOCK_STREAM is TCP
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("socket failed\n");
     }
 
-    // bin dthe server
+    // bind the socket with specified ip address and port
     if ((bind(server_socket, (struct sockaddr *)&server_address,
               sizeof(server_address))) < 0) {
         printf("failure on bind\n");
@@ -53,6 +69,8 @@ int main(int argc, char *argv[]) {
     listen(server_socket, MAX_CLIENTS);
 
     while (1) {
+        // accept waits for client connections
+        // if client connects we return a new socket descripter (client socket)
         if ((client_socket =
                  accept(server_socket, (struct sockaddr *)&server_address,
                         (socklen_t *)&addr_len)) < 0) {
@@ -60,10 +78,15 @@ int main(int argc, char *argv[]) {
             return 0;
         }
 
+        // receive data and store it in buf
         while (((buf_len) = recv(client_socket, buf, sizeof(buf), 0))) {
+            // print message to console
             fputs(buf, stdout);
+            // send received message back to client
             send(client_socket, buf, strlen(buf) + 1, 0);
         }
+
+        close(client_socket);
     }
 
     close(server_socket);
